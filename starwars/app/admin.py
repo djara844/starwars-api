fdc67439cb41bc4899048002d988101b7bce1190
Diego Jaramillo <djaramillo@vibra.fm>
2022-11-18 09:55:06 -0500

    Configure migrations and register models in admin-django :card_file_box:

--- starwars/app/admin.py
+++ starwars/app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.models import Character, Film, Planet
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Film)
+admin.site.register(Planet)
